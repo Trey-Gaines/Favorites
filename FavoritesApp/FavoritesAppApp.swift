dde6aa71be3a529ba84b105277f93bbe8371440a
Trey G <dove-ascent-0q@icloud.com>
2025-07-14 17:26:51 -0400

    Initial Commit

---
+++ FavoritesApp/FavoritesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesAppApp.swift
+//  FavoritesApp
+//
+//  Created by Trey Gaines on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FavoritesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FavoritesAppTests/FavoritesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesAppTests.swift
+//  FavoritesAppTests
+//
+//  Created by Trey Gaines on 7/14/25.
+//
+
+import Testing
+
+struct FavoritesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
